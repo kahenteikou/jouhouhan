0e0fa2f2d4903a560c8f26d6a09310c881df33e1
kokkiemouse <kokkiemouse@fascode.net>
2021-05-02 21:49:13 +0900

    added1
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,7 @@ class Header extends React.Component{
                 <div className={styles.Headerkun} >
                     <div className={styles.official_button}>
                         <a className={styles.official_button_a} href="/">
-                            <h1>福島高校SS部情報班</h1>
+                            <div className={styles.headerbig}>福島高校SS部情報班</div>
                         </a>
                     </div>
                 </div>
--- pages/about.jsx
+++ pages/about.jsx
@@ -12,7 +12,26 @@ class AboutPage extends React.Component{
           <div className={styles.About_Page_Gaiyou}>
               情報班について
           </div>
+          <div>
+            <center>
+              <h1>情報班とは?</h1>
+            </center>
+            <div className={styles.Hangaiyou}>
+            情報班はSS部にある班です。<br />
+            普段はタブレットPC室で元気に活動しています。
+            </div>
+            <center>
+              <h1>情報班の成り立ち</h1>
+            </center>
+            <div className={styles.Hangaiyou}>
+              元々は掲示板を作ろうとして立ち上がった班だそうです(先輩談)<br />
+              その後計画が挫折しましたがIT関係は残そうということでこのようになったらしいです(先輩談)
+            </div>
+          </div>
         </div>
+        <div style={{
+          marginTop:"15vw"
+        }} />
       </>
     )
   }
